dec2176fb4a817e454f244eee6b18f279222a1a3
Karol Piskorz <karolpiskorz8@gmail.com>
2024-02-23 13:29:25 +0100

    add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["templates/**/*.templ"],
+  content: ["internal/templates/**/*.templ"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
